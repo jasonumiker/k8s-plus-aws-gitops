afabb8062518315a546e7728959b14b6b7e5f4c7
Umiker <jumiker@38f9d3cc3502.ant.amazon.com>
2019-11-10 00:23:26 +1100

    Added install steps for prereqs

--- deploy-all.sh
+++ deploy-all.sh
@@ -1,4 +1,13 @@
 #!/bin/bash
+sudo curl --silent --location -o /usr/local/bin/kubectl "https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/kubectl"
+sudo chmod +x /usr/local/bin/kubectl
+sudo curl --silent --location -o /usr/local/bin/aws-iam-authenticator "https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator"
+sudo chmod +x /usr/local/bin/aws-iam-authenticator
+sudo curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
+sudo mv -v /tmp/eksctl /usr/local/bin
+sudo curl -L https://git.io/get_helm.sh | bash
+sudo yum install jq -y
+ssh-keygen -t rsa -N '' -f ~/.ssh/id_rsa
 export AWS_DEFAULT_REGION=ap-southeast-2
 chmod -R u+x *.sh
 cd aws-infrastructure
