754a0f208e1824a2b0ab1c49166b7304440f2481
Umiker <jumiker@38f9d3cc3502.ant.amazon.com>
2019-11-10 23:04:34 +1100

    Adding CDK Pipeline

---
+++ aws-infrastructure/aws-app-resources-pipeline.py
@@ -0,0 +1,66 @@
+from aws_cdk import (
+    aws_codebuild as codebuild,
+    aws_iam as iam,
+    aws_codepipeline as codepipeline,
+    aws_codepipeline_actions as codepipeline_actions,
+    core
+)
+
+class AWSAppResourcesPipeline(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+
+        # Create IAM Role For CodeBuild
+        codebuild_role = iam.Role(
+            self, "BuildRole",
+            assumed_by=iam.ServicePrincipal("codebuild.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("AdministratorAccess")
+            ]
+        )
+
+        # Create CodeBuild PipelineProject
+        build_project = codebuild.PipelineProject(
+            self, "BuildProject",
+            role=codebuild_role,
+            build_spec=codebuild.BuildSpec.from_source_filename("aws-app-resources/buildspec.yml")
+        )
+
+        # Create CodePipeline
+        pipeline = codepipeline.Pipeline(
+            self, "Pipeline"
+        )
+
+        # Create Artifact
+        artifact = codepipeline.Artifact()
+
+        # Add Source Stage
+        pipeline.add_stage(
+            stage_name="Source",
+            actions=[
+                codepipeline_actions.GitHubSourceAction(
+                    action_name="SourceCodeRepo",
+                    owner="jasonumiker",
+                    repo="k8s-plus-aws-gitops",
+                    output=artifact
+                )
+            ]
+        )
+
+        # Add CodeBuild Stage
+        pipeline.add_stage(
+            stage_name="Deploy",
+            actions=[
+                codepipeline_actions.CodeBuildAction(
+                    action_name="CodeBuildProject",
+                    project=build_project,
+                    type=codepipeline_actions.CodeBuildActionType.BUILD,
+                    input=artifact
+                )
+            ]
+        )
+
+app = core.App()
+AWSAppResourcesPipeline(app, "AWSAppResourcesPipeline")
+app.synth()
