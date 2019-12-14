d328438b06d1caea2f8b45860642f7aec33c4926
EC2 Default User <ec2-user@ip-10-0-8-64.ap-northeast-1.compute.internal>
2019-12-14 09:15:33 +0000

    Added u+x to install-flux.sh

--- k8s-infrastructure/install-flux.sh
+++ k8s-infrastructure/install-flux.sh
@@ -6,5 +6,4 @@ fluxctl install \
 --git-email=${GHUSER}@users.noreply.github.com \
 --git-url=git@github.com:${GHUSER}/k8s-plus-aws-gitops \
 --git-path=k8s-app-resources \
---git-readonly=true \
 --namespace=default | kubectl apply -f -
