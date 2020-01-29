6962663d5989def6337d686095a5d21f6bcd809f
Jason Umiker <jason.umiker@gmail.com>
2020-01-29 19:53:45 +1100

    Fixed typo

--- k8s-infrastructure/install-external-secrets.sh
+++ k8s-infrastructure/install-external-secrets.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 helm repo add external-secrets https://godaddy.github.io/kubernetes-external-secrets/
-helm install --wait --timeout 120 --set env.AWS_REGION=$AWS_DEFAULT_REGION --set serviceAccount.name=kubernetes-external-secrets \
+helm install --wait --timeout 120s --set env.AWS_REGION=$AWS_DEFAULT_REGION --set serviceAccount.name=kubernetes-external-secrets \
 --set serviceAccount.create=false --set securityContext.fsGroup=65534 \
 external-secrets/kubernetes-external-secrets --generate-name
