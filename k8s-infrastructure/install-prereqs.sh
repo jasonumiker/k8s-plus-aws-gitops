d2a4c6269119991d67c159ff4cb7f05905bb6a06
EC2 Default User <ec2-user@ip-10-0-8-64.ap-northeast-1.compute.internal>
2020-03-22 11:08:47 +0000

    Updated ALB Ingress Controller

--- k8s-infrastructure/install-prereqs.sh
+++ k8s-infrastructure/install-prereqs.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-sudo curl --silent --location -o /usr/local/bin/kubectl "https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/kubectl"
+sudo curl --silent --location -o /usr/local/bin/kubectl "https://amazon-eks.s3-us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/kubectl"
 sudo chmod +x /usr/local/bin/kubectl
-sudo curl --silent --location -o /usr/local/bin/aws-iam-authenticator "https://amazon-eks.s3-us-west-2.amazonaws.com/1.14.6/2019-08-22/bin/linux/amd64/aws-iam-authenticator"
+sudo curl --silent --location -o /usr/local/bin/aws-iam-authenticator "https://amazon-eks.s3-us-west-2.amazonaws.com/1.15.10/2020-02-22/bin/linux/amd64/aws-iam-authenticator"
 sudo chmod +x /usr/local/bin/aws-iam-authenticator
 sudo curl --silent --location "https://github.com/weaveworks/eksctl/releases/download/latest_release/eksctl_$(uname -s)_amd64.tar.gz" | tar xz -C /tmp
 sudo mv -v /tmp/eksctl /usr/local/bin
-sudo curl --location -o /usr/local/bin/fluxctl "https://github.com/fluxcd/flux/releases/download/1.16.0/fluxctl_linux_amd64"
+sudo curl --silent --location -o /usr/local/bin/fluxctl "https://github.com/fluxcd/flux/releases/download/1.18.0/fluxctl_linux_amd64"
 sudo chmod +x /usr/local/bin/fluxctl
 sudo curl -L https://raw.githubusercontent.com/helm/helm/master/scripts/get-helm-3 | bash
 sudo yum install jq -y
