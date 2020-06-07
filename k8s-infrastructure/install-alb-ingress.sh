434565ce85779ff9d3aef3834235f28065480f67
Jason Umiker <jason.umiker@gmail.com>
2020-06-07 15:37:22 +1000

    Updated several components and moved to Sydney Region

--- k8s-infrastructure/install-alb-ingress.sh
+++ k8s-infrastructure/install-alb-ingress.sh
@@ -2,8 +2,8 @@
 aws cloudformation list-exports --region $AWS_DEFAULT_REGION > exports.json
 VPCID=$(cat exports.json | jq -r '.Exports[] | select(.Name | contains ("VPC")) | .Value')
 rm exports.json
-kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.5/docs/examples/rbac-role.yaml
-curl https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.5/docs/examples/alb-ingress-controller.yaml | 
+kubectl apply -f https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.8/docs/examples/rbac-role.yaml
+curl https://raw.githubusercontent.com/kubernetes-sigs/aws-alb-ingress-controller/v1.1.8/docs/examples/alb-ingress-controller.yaml | 
 sed "s/# - --cluster-name=devCluster/- --cluster-name=cluster/;s/# - --aws-region=us-west-1/- --aws-region=$AWS_DEFAULT_REGION/" |
 sed "s/# - --aws-vpc-id=vpc-xxxxxx/- --aws-vpc-id=$VPCID/" |
 kubectl apply -f -
