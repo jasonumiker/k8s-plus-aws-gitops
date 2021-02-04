fcb53db2b776fda8407d88c2996d1181507e4508
Jason Umiker <jason.umiker@gmail.com>
2021-02-05 10:37:27 +1100

    Started addition of a container build component

--- aws-infrastructure/infrastructure-stacks.py
+++ aws-infrastructure/infrastructure-stacks.py
@@ -410,7 +410,64 @@ class AWSAppResourcesPipeline(core.Stack):
             ]
         )
 
+class DockerBuildPipeline(core.Stack):
+
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+        
+        # Create IAM Role For CodeBuild
+        codebuild_role = iam.Role(
+            self, "BuildRole",
+            assumed_by=iam.ServicePrincipal("codebuild.amazonaws.com"),
+            managed_policies=[
+                iam.ManagedPolicy.from_aws_managed_policy_name("AdministratorAccess")
+            ]
+        )
+
+        # Create CodeBuild PipelineProject
+        build_project = codebuild.PipelineProject(
+            self, "BuildProject",
+            role=codebuild_role,
+            build_spec=codebuild.BuildSpec.from_source_filename("dockerbuild/buildspec.yml")
+        )
+
+        # Create CodePipeline
+        pipeline = codepipeline.Pipeline(
+            self, "Pipeline"
+        )
+
+        # Create Artifact
+        artifact = codepipeline.Artifact()
+
+        # Add Source Stage
+        pipeline.add_stage(
+            stage_name="Source",
+            actions=[
+                codepipeline_actions.GitHubSourceAction(
+                    action_name="SourceCodeRepo",
+                    owner="jasonumiker",
+                    repo="k8s-plus-aws-gitops",
+                    output=artifact,
+                    oauth_token=core.SecretValue.secrets_manager('github-token')
+                )
+            ]
+        )
+
+        # Add CodeBuild Stage
+        pipeline.add_stage(
+            stage_name="Deploy",
+            actions=[
+                codepipeline_actions.CodeBuildAction(
+                    action_name="CodeBuildProject",
+                    project=build_project,
+                    type=codepipeline_actions.CodeBuildActionType.BUILD,
+                    input=artifact
+                )
+            ]
+        )
+
 app = core.App()
 aws_infrastructure_stack = AWSInfrastructureStack(app, "AWSInfrastructureStack")
 resources_pipeline_stack = AWSAppResourcesPipeline(app, "ResourcesPipelineStack")
+docker_build_pipeline_stack = AWSAppResourcesPipeline(app, "DockerBuildPipeline")
 app.synth()
---
+++ dockerbuild/README.md
@@ -0,0 +1,5 @@
+# Building the container
+
+Ghost provides a pre-built container image but I wanted to illustrate how to have a pipeline that would build the container as part of this demo.
+
+Dockerfile and docker-entrypoint.sh cloned from https://hub.docker.com/_/ghost/ on 4/2/21
